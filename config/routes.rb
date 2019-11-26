c8e66dc7eeae2c86f6d92ecbf51cd0e8a94c1e60
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-11-26 20:37:41 +0000

    updated contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'what_it_is', to: 'pages#what_it_is'
   get 'research', to: 'pages#research'
+   get 'contact', to: 'pages#contact'
 
-  resources :contacts, only: [:new, :create]
+  # resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
